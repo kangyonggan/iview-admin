f026c51a50929b7536e856a9f8be61cd99239f3e
康永敢 <kangyonggan@gmail.com>
2018-05-31 07:45:45 +0800

    uat

--- config/url.js
+++ config/url.js
@@ -1,6 +1,7 @@
 import env from './env'
 
 const DEV_URL = 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/'
-const PRO_URL = 'https://produce.com'
+const UAT_URL = 'http://localhost:8027/'
+const PRO_URL = 'http://kangyonggan/'
 
-export default env === 'development' ? DEV_URL : PRO_URL
+export default env === 'development' ? DEV_URL : env === 'uat' ? UAT_URL : PRO_URL
