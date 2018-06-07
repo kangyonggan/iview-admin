08e901c9f764080eed5fa973f00614370b4ed878
kangyonggan <kangyonggan@gmail.com>
2018-06-07 16:40:34 +0800

    prod

--- config/url.js
+++ config/url.js
@@ -2,6 +2,6 @@ import env from './env'
 
 const DEV_URL = 'https://kangyonggan/'
 const UAT_URL = 'http://localhost:8027/'
-const PRO_URL = 'http://kangyonggan:8027/'
+const PRO_URL = 'https://kangyonggan/'
 
 export default env === 'development' ? DEV_URL : env === 'uat' ? UAT_URL : PRO_URL
