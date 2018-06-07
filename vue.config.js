596e0f59b1a0dbce86b6b810d907cd4cab9ec823
kangyonggan <kangyonggan@gmail.com>
2018-06-07 16:40:11 +0800

    prod

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const resolve = dir => {
 // 这里需要修改为你线上项目存放的路径
 // 比如你打包的文件放到服务器的my-app文件夹，域名为a.com，则应改为
 // http(s)://a.com/my-app/
-const BASE_URL = 'http://kangyonggan.com:8088/'
+const BASE_URL = 'http://kangyonggan.com:8027/'
 
 module.exports = {
   // Project deployment base
