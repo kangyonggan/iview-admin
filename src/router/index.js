479fea24e7d136fd8679c1a66b0d36449eafca02
kangyonggan <kangyonggan@gmail.com>
2018-06-08 09:54:02 +0800

    log

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,8 @@ const LOGIN_PAGE_NAME = 'login'
 const IS_LOCKED = getLockStatus()
 
 router.beforeEach((to, from, next) => {
+  console.log('router beforeEach:')
+  console.log(to)
   iView.LoadingBar.start()
   if (IS_LOCKED && to.name !== LOCK_PAGE_NAME) {
     // 当前是锁定状态并且用户要跳转到的页面不是解锁页面
@@ -61,6 +63,8 @@ router.beforeEach((to, from, next) => {
 })
 
 router.afterEach(to => {
+  console.log('router afterEach:')
+  console.log(to)
   iView.LoadingBar.finish()
   window.scrollTo(0, 0)
 })
