90f2644d48bfe3369ad9623e68bca46ee3e6cb52
kangyonggan <kangyonggan@gmail.com>
2018-06-08 09:24:48 +0800

    api

--- config/url.js
+++ config/url.js
@@ -1,7 +1,7 @@
 import env from './env'
 
 const DEV_URL = 'https://kangyonggan.com/'
-const UAT_URL = 'http://localhost:8027/'
+const UAT_URL = 'http://localhost:8027/api/'
 const PRO_URL = 'https://kangyonggan.com/api/'
 
 export default env === 'development' ? DEV_URL : env === 'uat' ? UAT_URL : PRO_URL
